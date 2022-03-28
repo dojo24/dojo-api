c2efa78baae0937e52e7d9886573295e36e08458
Melissa Longenberger <mlongenberger@codingdojo.com>
2022-03-28 18:57:47 -0400

    fixing files

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const server = require('./utils/server')
-
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT, () => {
-    console.log(`Listening on ` + PORT)
-})
